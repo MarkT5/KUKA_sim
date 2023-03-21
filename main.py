dccc30bca539d9801677bda27556836dce4ab54b
ZaSKaR <mark.turkov000@gmail.com>
2023-03-21 22:45:05 +0300

    movement uncertainty PRO

--- main.py
+++ main.py
@@ -102,41 +102,62 @@ def get_c_space(bin_map, c_space):
 
 
 def get_transition_matrix(transition_vector):
-    transition_matrix = np.zeros((5, 5, 5))
-    ind = tuple((np.ones((3)) * 2 + transition_vector).astype(np.int32))
-    transition_matrix[ind] = 1
-    mov_prob_dist = np.ones((3, 3, 3)) / 200
-    mov_prob_dist[:, 1, 1] = 1 / 60
-    mov_prob_dist[1, :, 1] = 1 / 60
-    mov_prob_dist[1, 1, :] = 1 / 60
-    mov_prob_dist[1, 1, 1] = 0
-
-    mov_prob_dist[1, 1, 1] = 1 - np.sum(mov_prob_dist)
-
-    transition_matrix = scipy.ndimage.convolve(transition_matrix, mov_prob_dist)
+    transition_matrix = np.zeros((5, 5))
+    # ind = tuple((np.ones((2)) * 2 + transition_vector).astype(np.int32))
+    # transition_matrix[ind] = 1
+    # mov_prob_dist = np.ones((3, 3, 3)) / 200
+    # mov_prob_dist[:, 1, 1] = 1 / 60
+    # mov_prob_dist[1, :, 1] = 1 / 60
+    # mov_prob_dist[1, 1, :] = 1 / 60
+    # mov_prob_dist[1, 1, 1] = 0
+    # mov_prob_dist[1, 1, 1] = 1 - np.sum(mov_prob_dist)
+    for i in range(5):
+        for j in range(5):
+            k = 1
+            if i == 0 or j == 0 or i == 4 or j == 4:
+                k = 0.2
+            v = np.array((i, j)) - np.ones(2) * 2
+            len_dif = abs(np.linalg.norm(transition_vector) - np.linalg.norm(v))
+            if i == 2 and j == 2:
+                transition_matrix[i, j] = (6 - len_dif * 3)
+                continue
+            transition_matrix[i, j] = ((1 + np.dot(v, transition_vector) / (
+                    np.linalg.norm(transition_vector) * np.linalg.norm(v))) / 2) * (6 - len_dif * 3)
+    transition_matrix = transition_matrix / np.sum(transition_matrix)
     return transition_matrix
 
 
+def apply_movement_uncertainty_on_layer(extended_prob_arr, transition_vector, ang):
+    rot = np.array([[math.cos(ang), -math.sin(ang)],
+                    [math.sin(ang), math.cos(ang)]])
+    new_vect = transition_vector @ rot
+    return scipy.ndimage.convolve(extended_prob_arr,
+                                  get_transition_matrix(new_vect))
+
+
 def apply_movement_uncertainty(prob_arr, transition_vector):
-    enhanced_prob_arr = np.concatenate((prob_arr[:, :, -5:], prob_arr, prob_arr[:, :, :5]), axis=2)
+    extended_prob_arr = np.concatenate((prob_arr[:, :, -5:], prob_arr, prob_arr[:, :, :5]), axis=2)
     if transition_vector[2] > ang_res / 2:
         transition_vector[2] -= ang_res
     if transition_vector[2] < -ang_res / 2:
         transition_vector[2] += ang_res
     if (np.abs(transition_vector) > 1).any():
-        mult = np.max(np.abs(transition_vector))
+        mult = np.max(np.abs(transition_vector[:2]))
         step = transition_vector / mult
         prev_step = np.zeros_like(step)
 
-        for i in range(mult):
-            prob_arr = scipy.ndimage.convolve(enhanced_prob_arr,
-                                              get_transition_matrix((step * i - prev_step).astype(int)))
+        for i in range(1, int(mult)+1):
+            for ang in range(extended_prob_arr.shape[2]):
+                extended_prob_arr[:, :, ang] = apply_movement_uncertainty_on_layer(extended_prob_arr[:, :, ang],
+                                                                          (step * i - prev_step)[:2], to_radians(ang))
             prev_step = step * i
-        return prob_arr[:, :, 5:-5]
+        return extended_prob_arr[:, :, 5:-5]
     else:
-        out = scipy.ndimage.convolve(enhanced_prob_arr,
-                                     get_transition_matrix(transition_vector))[:, :, 5:-5]
-        return out
+        for ang in range(extended_prob_arr.shape[2]):
+            extended_prob_arr[:, :, ang] = apply_movement_uncertainty_on_layer(extended_prob_arr[:, :, ang],
+                                                                               transition_vector[:2],
+                                                                               to_radians(ang))
+        return extended_prob_arr[:, :, 5:-5]
 
 
 class Robot:
@@ -171,13 +192,14 @@ class Robot:
 
 last_checked_pressed_keys = []
 move_speed_val = 0.01 * scale
+curr_speed = np.array([0.0, 0.0, 0.0])
 
 
 def update_keys(screen):
     global last_checked_pressed_keys
     global out
     global prob_arr
-    global out_full
+    global curr_speed
     pressed_keys = screen.pressed_keys
     move_speed = np.array([0, 0, 0.0])
     fov = 0
@@ -202,18 +224,15 @@ def update_keys(screen):
     move_speed[1] = side * move_speed_val
 
     if pg.K_c in pressed_keys:
-        move_speed = [0,0,0]
-        robot.move_base(0,0,0)
+        move_speed = [0, 0, 0]
+        robot.move_base(0, 0, 0)
         calc_txt.visible = True
         calc_txt2.visible = True
         screen.wait_step()
         screen.wait_step()
-        print("begin")
         get_c_space(map_editor.get_bin_map(), robot.c_space)
         prob_arr = get_probability_distribution(robot.lidar, robot.c_space, prob_arr)
         out = prob_arr[:, :, int(to_descrete(robot.increment[2]))]
-        print("end")
-        out_full = prob_arr
         calc_txt.visible = False
         calc_txt2.visible = False
 
@@ -225,16 +244,17 @@ def update_keys(screen):
         prob_arr = get_probability_distribution(robot.lidar, robot.c_space, prob_arr)
         calc_txt.visible = False
         calc_txt2.visible = False
-        out_full = prob_arr
 
     if last_checked_pressed_keys != pressed_keys:
         robot.move_base(*move_speed)
+        curr_speed = np.array(move_speed)
         last_checked_pressed_keys = pressed_keys[:]
 
 
 def out_prob_mat():
-    out = out_full[:, :, curr_prob_ang]
-    ret = (np.stack((out.T, out.T, out.T), axis=2)) * 255/np.max(out_full)
+    out = prob_arr[:, :, curr_prob_ang]
+    dev = max(np.max(prob_arr), 0.00000000001)
+    ret = (np.stack((out.T, out.T, out.T), axis=2)) * 255 / dev
     return ret
 
 
@@ -261,8 +281,6 @@ def get_robot_pos_txt():
 prob_arr = np.zeros((*map_size, ang_res)).astype(np.float64)
 
 out = prob_arr[:, :, int(to_descrete(robot.increment[2]))]
-out_full = prob_arr
-
 
 # GUI layout
 screen = Screen(600, 800)
@@ -275,9 +293,9 @@ Text(screen, name="slider_label", inp_text=lambda *args: "angle in prob map:", x
 Text(screen, name="slider_val_label", inp_text=get_prob_ang_txt, x=0.93, y=0.97)
 Text(screen, name="robot_pos", inp_text=get_robot_pos_txt, x=0.01, y=0.01, color=(0, 0, 0))
 calc_txt = Text(screen, name="Calculating", inp_text=lambda *args: "Calculating...",
-     x=0.25, y=0.6, font_size=40, color=(255, 0, 0))
+                x=0.25, y=0.6, font_size=40, color=(255, 0, 0))
 calc_txt2 = Text(screen, name="Calculating2", inp_text=lambda *args: "(this may take a while)",
-     x=0.02, y=0.67, font_size=40, color=(255, 0, 0))
+                 x=0.02, y=0.67, font_size=40, color=(255, 0, 0))
 robot_sp = RobotSprite(screen, name="robot", x=0.5, y=0.5, color=(0, 255, 0))
 robot_sp_guess = RobotSprite(screen, name="robot_guess", x=0.5, y=0.5, color=(0, 0, 255))
 calc_txt.visible = False
@@ -285,27 +303,32 @@ calc_txt2.visible = False
 robot_sp.par_surf = map_editor
 robot_sp_guess.par_surf = map_editor
 
-
-#screen.add_fps_indicator(color=(0, 0, 0))
-robot_discrete_pos = np.array((*(robot.increment[:2] * scale).astype(int), int(to_descrete(robot.increment[2]))))
-old_robot_discrete_pos = robot_discrete_pos
-
 # prob_arr[int(robot.increment[0] * scale), int(robot.increment[1] * scale), int(to_descrete(robot.increment[2]))] = 1
 prob_arr[:, :, :] = 1
 prob_arr = prob_arr / np.sum(prob_arr)
 
+prev_t = time.time()
+curr_shift = np.array([0.0, 0.0])
+ang_chng = 0
+
 while True:
-    robot_sp.dec_pos = (robot.increment[:2] * scale * mat_scale_factor).astype(int)
-    robot_sp.ang = robot.increment[2]
+    robot_sp.dec_pos = (np.array(robot.increment[:2]) * scale * mat_scale_factor).astype(int)
+    robot_sp.ang = -robot.increment[2]
     curr_guess = np.array(np.unravel_index(np.argmax(prob_arr, axis=None), prob_arr.shape))
     robot_sp_guess.dec_pos = (curr_guess[:2] * mat_scale_factor).astype(int)
     robot_sp_guess.ang = to_radians(curr_guess[2])
-    if (robot_discrete_pos != old_robot_discrete_pos).any():
-        prob_arr = apply_movement_uncertainty(prob_arr, robot_discrete_pos - old_robot_discrete_pos)
-        out_full = prob_arr
-        old_robot_discrete_pos = np.copy(robot_discrete_pos)
-        print(np.sum(out_full))
-    robot_discrete_pos = np.array((*(robot.increment[:2] * scale).astype(int), int(to_descrete(robot.increment[2]))))
+    print(curr_shift.shape, curr_speed.shape)
+    curr_shift += curr_speed[:2] * int((time.time() - prev_t) * scale * 100)
+    ang_chng += curr_speed[2] * (time.time() - prev_t)*90
+    prev_t = time.time()
+    if (curr_shift > 1).any():
+        prob_arr = apply_movement_uncertainty(prob_arr, (*curr_shift, ang_chng))
+        curr_shift = np.array([0.0, 0.0])
+    if abs(ang_chng) > 1:
+        prob_arr = np.concatenate((prob_arr[:, :, -1*int(ang_chng):], prob_arr[:, :, :-1*int(ang_chng)]), axis=2)
+        ang_chng -= int(ang_chng)
+        if time.time() - prev_t > 1:
+            curr_speed[2] = 0
     robot.bin_map = map_editor.get_bin_map()
     update_keys(screen)
     time.sleep(0.01)
